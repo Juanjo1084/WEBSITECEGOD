e028b3820e6a9673369605c865b4ee6451905a3e
Juanjo1084 <64788744+Juanjo1084@users.noreply.github.com>
2025-03-05 21:39:49 -0500

    pull

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -5,11 +5,11 @@ function LandingPage() {
         <>
         <div className="landing-container">
             <h1 className="landing-title">Bienvenido a CEGOD Ingeniería</h1>
-            {/* <img
-            src="./public/images/logo-Photoroom.png"
+            <img
+            src="https://img.freepik.com/vector-gratis/diseno-logotipo-energia_23-2150045657.jpg?t=st=1741228772~exp=1741232372~hmac=f772a83753719b728cd33d0060b23b5e070e3c99e52477c8e9f562cd59912e2d&w=740"
             alt="CEGOD Ingeniería Logo"
             className="landing-logo"
-            /> */}
+            />
         </div>
         </>
     )
